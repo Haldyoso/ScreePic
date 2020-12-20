b5cfa57cc112adfa08c0597ed2469a1c0bdb5c26
MartinHalus <halus.martin@gmail.com>
2020-12-20 22:46:33 +0100

    static Splash screen done
    https://www.youtube.com/watch?v=GqcuVipcaOE&ab_channel=CodeWithMazn&t=145s

---
+++ app/src/main/java/com/maha/screepic/SplashScreen.kt
@@ -0,0 +1,12 @@
+package com.maha.screepic
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreen: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+    }
+}
