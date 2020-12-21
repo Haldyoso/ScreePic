dc75845f747181ca8fc9b22adb7103097850337c
MartinHalus <halus.martin@gmail.com>
2020-12-21 20:05:11 +0100

    static Splash screen done Animation not wokring - not able to import tvBrant to SplashScreen.kt
    https://www.youtube.com/watch?v=GqcuVipcaOE&ab_channel=CodeWithMazn&t=435s

--- app/src/main/java/com/maha/screepic/SplashScreen.kt
+++ app/src/main/java/com/maha/screepic/SplashScreen.kt
@@ -1,12 +1,32 @@
 package com.maha.screepic
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.os.PersistableBundle
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+
 
 class SplashScreen: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_screen)
+
+        val botAnimation = AnimationUtils.loadAnimation(this,R.anim.bot_animation)
+
+
+        tvBrand.startAnimation(botAnimation)
+
+        val splashScreenTimeOut = 4000
+        val homeIntent = Intent(this@SplashScreen, MainActivity::class.java )
+
+        Handler().postDelayed({
+            startActivity(homeIntent)
+            finish()
+        }, splashScreenTimeOut.toLong() )
+
+
     }
 }
