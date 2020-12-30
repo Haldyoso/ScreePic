d5bfa36bf89ca7bd26045a818027b3ed78ae3a17
MartinHalus <halus.martin@gmail.com>
2020-12-30 22:06:08 +0100

    1st button re-write textview

--- app/src/main/java/com/maha/screepic/MainActivity.kt
+++ app/src/main/java/com/maha/screepic/MainActivity.kt
@@ -2,12 +2,39 @@ package com.maha.screepic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import org.w3c.dom.Text
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_ScreePic)
         setContentView(R.layout.activity_main)
+        findViewById<ImageButton>(R.id.buttonChangeDisplayText).setOnClickListener{
+            findViewById<TextView>(R.id.textViewDisplayTextChange).text = "nieco"
+        }
+
+
+
+
+
 
     }
+
+
+
+
+
+
+
+
+
+
 }
