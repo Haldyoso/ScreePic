7178bf1a68ee063ca68d1f60239bbe88b5481bf5
MartinHalus <halus.martin@gmail.com>
2021-01-10 18:26:34 +0100

    working backup before migrate to AndroidX

--- app/src/main/java/com/maha/screepic/MainActivity.kt
+++ app/src/main/java/com/maha/screepic/MainActivity.kt
@@ -2,13 +2,20 @@ package com.maha.screepic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import android.widget.ImageButton
+import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
+
+    private var progressBar: ProgressBar? = null
+    private var i = 0
+    private var txtView: TextView? = null
+    private val handler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
