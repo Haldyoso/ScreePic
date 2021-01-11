1bd62827e4f266fe4acc5ec3afa327580573da3e
MartinHalus <halus.martin@gmail.com>
2021-01-11 19:43:50 +0100

    recycle view container basic

--- app/src/main/java/com/maha/screepic/MainActivity.kt
+++ app/src/main/java/com/maha/screepic/MainActivity.kt
@@ -3,11 +3,14 @@ package com.maha.screepic
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.provider.ContactsContract
 import android.view.View
 import android.widget.ImageButton
 import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,10 +20,16 @@ class MainActivity : AppCompatActivity() {
     private var txtView: TextView? = null
     private val handler = Handler()
 
+    private lateinit var blogAdapter: BlogRecyclerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_ScreePic)
         setContentView(R.layout.activity_main)
+
+        initRecyclerView()
+        addDataSet()
+
 
     }
 
@@ -29,6 +38,21 @@ class MainActivity : AppCompatActivity() {
         findViewById<TextView>(R.id.textMenu).text = view.tag.toString()
     }
 
+    private fun  addDataSet(){
+        val data = DataSource.createDataSet()
+        blogAdapter.submitList(data)
+    }
+    private fun initRecyclerView(){
+        findViewById<RecyclerView>(R.id.recycler_view).apply {
+
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            val topSpacingDecoration = TopSpacingItemDecoration (30)
+            addItemDecoration(topSpacingDecoration)
+            blogAdapter = BlogRecyclerAdapter()
+            adapter = blogAdapter
+        }
+
+    }
 
 
 
---
+++ app/src/main/java/com/maha/screepic/models/BlogPost.kt
@@ -0,0 +1,11 @@
+package com.maha.screepic.models
+
+data class BlogPost(
+
+    var title: String,
+    var body: String,
+    var image: String,
+    var username: String,
+
+) {
+}
